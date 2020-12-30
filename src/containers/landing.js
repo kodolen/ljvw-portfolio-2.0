de70f94189e9efda330c838dc42a1ed4ed0d6ab8
Lennart van Welzen <34541619+kodolen@users.noreply.github.com>
2020-12-30 15:37:26 +0100

    Added landing

--- src/containers/landing.js
+++ src/containers/landing.js
@@ -6,6 +6,7 @@ const Landing = () => {
     return (
         <div className="landing-container">
             <div className="landing-container__img-wrapper">
+                <div className="landing-container__overlay"></div>
                 <img className="landing-container__img" src={Img} alt="" />
             </div>
             <div className="landing-container__intro">
@@ -14,9 +15,9 @@ const Landing = () => {
                 <p className="landing-container__text">Hey! I’m Lennart. I'm living in Rotterdam. At this moment I’m looking for an internship as a front-end developer.</p>
             </div>
             <div className="landing-container__social-icons">
-                <FaGithub />
-                <FaLinkedinIn />
-                <FaEnvelope />
+                <a href="https://github.com/kodolen" target="_blank" rel="noreferrer"><FaGithub /></a>
+                <a href="https://www.linkedin.com/in/lennart-van-welzen-b4ab42134/" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+                <a href="mailto:lennartjvw@icloud.com" target="_blank" rel="noreferrer"><FaEnvelope /></a>
             </div>
         </div>
     );
