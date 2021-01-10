334fd3e6331caf1235b8aca235304a19630fd0ca
Lennart van Welzen <34541619+kodolen@users.noreply.github.com>
2021-01-10 14:20:23 +0100

    added cases

--- src/containers/about.js
+++ src/containers/about.js
@@ -13,9 +13,9 @@ const About = () => {
           <div className="col-xs-12 col-sm-10 off-sm-1 col-md-4 off-md-2">
             <p className="about-container__quote">I love to work with the newest technologies</p>
           </div>
-          <div className="col-xs-12 col-sm-10 off-sm-1 col-md-4">
+          <div className="col-xs-12 col-sm-10 off-sm-1 col-md-3">
             <p className="about-container__text">I have graduated as a graphic and interactive designer at the Grafisch Lyceum Rotterdam and currently I’m following the study Creative Media and Game Technologies at the Rotterdam University of applied science. During this study I have worked on different projects in which I have worked with different technologies, frameworks and programming languages. <br></br><br></br>In the past years I've been heavily focusing on front-end development. At this moment I’m in my last year and I’m looking for a graduation internship. I’m also active as a freelance web designer and web developer which has a positive effect on my development as a developer.</p>
-            <span className="about-container__link">Want to know more? <a href="#" className="about-container__link--hover">send me a message</a></span>
+            <span className="about-container__link">Want to know more? <a href="#" className="about-container__link--hover link">send me a message</a></span>
           </div>
           <div className="col-xs-12 col-sm-10 off-sm-1 col-md-8 off-md-2">
             <p className="about-container__subtitle">I can work with:</p>
--- src/containers/cases.js
+++ src/containers/cases.js
@@ -1,10 +1,63 @@
+import W4m from '../assets/img/w4m.png'
+import Ljvw from '../assets/img/ljvw-dev.png'
+import W4mWeb from '../assets/img/w4m-web.png'
+import LjvwWeb from '../assets/img/ljvw-web.png'
+import RedKiwi from '../assets/img/redkiwi.png'
+import GL from '../assets/img/GL.png'
+
+import W4mLogo from '../assets/logo/logo-w4m.png'
+import LjvwDevLogo from '../assets/logo/ljvw-dev-logo.png'
+import RedKiwiLogo from '../assets/logo/rk-logo.png'
+import GlLogo from '../assets/logo/gl-logo.png'
+import { FaGithub, FaLinkedinIn, FaEnvelope } from 'react-icons/fa';
+
 const Cases = () => {
-    return (
-      <div className="landing-container">
-          <h1>Cases</h1>
+  return (
+    <div className="cases-container">
+      <div className="inner">
+        <div className="row">
+          <div className="col-xs-12 col-sm-10 off-sm-1 col-md-6 off-md-2">
+            <h2 className="cases-container__title">Cases</h2>
+            <p className="cases-container__text">In the past years I have worked on different projects. The projects shown below are school projects but also projects I did as an intern or a freelancer. I also have more projects on my Github page.</p>
+            <span className="about-container__link">Check out my <a href="#" className="about-container__link--hover link">Github page</a></span>
+          </div>
+          <div className="col-xs-12 col-sm-10 off-sm-1 col-md-8 off-md-2">
+            <div className="cases-container__wrapper">
+              <a href="https://work4media.nl/" target="_blank">
+                <div className="cases-container__overlay">
+                  <img src={W4mLogo} />
+                  <span className="cases-container__subtitle">Freelance</span>
+                </div>
+                <div className="cases-container__case" style={{ backgroundImage: `url(${W4mWeb})` }}></div>
+              </a>
+              <a href="https://develop.ljvw.nl/" target="_blank">
+              <div className="cases-container__overlay">
+                  <img src={LjvwDevLogo} />
+                  <span className="cases-container__subtitle">Freelance</span>
+                </div>
+                <div className="cases-container__case" style={{ backgroundImage: `url(${LjvwWeb})` }}></div>
+              </a>
+              <a href="https://store.steampowered.com/app/1203680/Guiding_Light/" target="_blank" className="no-margin-bot">
+              <div className="cases-container__overlay">
+                  <img src={RedKiwiLogo} />
+                  <span className="cases-container__subtitle">Internship</span>
+                </div>
+                <div className="cases-container__case" style={{ backgroundImage: `url(${RedKiwi})` }}></div>
+              </a>
+              <a href="https://work4media.nl/" target="_blank" className="no-margin-bot">
+              <div className="cases-container__overlay">
+                  <img src={GlLogo} />
+                  <span className="cases-container__subtitle">School project</span>
+                </div>
+                <div className="cases-container__case" style={{ backgroundImage: `url(${GL})` }}></div>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default Cases;
-  
+    </div>
+
+  );
+}
+
+export default Cases;
