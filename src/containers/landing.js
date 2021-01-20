7f3c7b994b61b873b00732b80e47cce42a3f3773
Lennart van Welzen <34541619+kodolen@users.noreply.github.com>
2021-01-20 16:49:06 +0100

    added fix

--- src/containers/landing.js
+++ src/containers/landing.js
@@ -5,15 +5,25 @@ import { FaGithub, FaLinkedinIn, FaEnvelope } from 'react-icons/fa';
 const Landing = () => {
     return (
         <div className="landing-container">
-            <div className="landing-container__img-wrapper">
-                <div className="landing-container__overlay"></div>
-                <img className="landing-container__img" src={Img} alt="" />
+            <div className="landing-container__intro_wrapper">
+                <div className="row full-height">
+                    <div className="col-xs-12 col-sm-10 off-sm-1 col-md-8 off-md-2 full-height">
+                        <div className="landing-container__intro_wrapper_inner">
+                            <div className="landing-container__img-wrapper">
+                                <div className="landing-container__overlay"></div>
+                                <img className="landing-container__img" src={Img} alt="" />
+                            </div>
+                            <div className="landing-container__intro">
+                                <span className="landing-container__subtitle">Lennart van Welzen</span>
+                                <h1 className="landing-container__title">FRONT-END DEVELOPER</h1>
+                                <p className="landing-container__text">Hey! I’m Lennart. I'm living in Rotterdam. At this moment I’m looking for an internship as a front-end developer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="landing-container__intro">
-                <span className="landing-container__subtitle">Lennart van Welzen</span>
-                <h1 className="landing-container__title">FRONT-END DEVELOPER</h1>
-                <p className="landing-container__text">Hey! I’m Lennart. I'm living in Rotterdam. At this moment I’m looking for an internship as a front-end developer.</p>
-            </div>
+
+
             <div className="landing-container__social-icons">
                 <a href="https://github.com/kodolen" target="_blank" rel="noreferrer"><FaGithub /></a>
                 <a href="https://www.linkedin.com/in/lennart-van-welzen-b4ab42134/" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
